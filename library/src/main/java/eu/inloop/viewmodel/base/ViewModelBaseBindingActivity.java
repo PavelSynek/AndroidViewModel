abdc83675456a0013a8bcf4984ff148b8ff3b244
PavelSynek <pavel.synek@gmail.com>
2017-06-26 21:43:05 +0200

    Add missing method

--- library/src/main/java/eu/inloop/viewmodel/base/ViewModelBaseBindingActivity.java
+++ library/src/main/java/eu/inloop/viewmodel/base/ViewModelBaseBindingActivity.java
@@ -1,18 +1,33 @@
 package eu.inloop.viewmodel.base;
 
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import eu.inloop.viewmodel.AbstractViewModel;
 import eu.inloop.viewmodel.IView;
 
-public abstract class ViewModelBaseBindingActivity<T extends IView, R extends AbstractViewModel<T>> extends ViewModelBaseActivity<T, R> implements IView {
+public abstract class ViewModelBaseBindingActivity<T extends IView, R extends AbstractViewModel<T>, B extends ViewDataBinding>
+		extends ViewModelBaseActivity<T, R>
+		implements IView {
 
 	@CallSuper
 	@Override
 	protected void onCreate(@Nullable final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getViewModelHelper().performBinding(this);
+	}
+
+	@SuppressWarnings("unused")
+	@NonNull
+	public B getBinding() {
+		try {
+			return (B) getViewModelHelper().getBinding();
+		} catch (ClassCastException ex) {
+			throw new IllegalStateException("Method getViewModelBindingConfig() has to return same " +
+					"ViewDataBinding type as it is set to base Fragment");
+		}
 	}
 }
